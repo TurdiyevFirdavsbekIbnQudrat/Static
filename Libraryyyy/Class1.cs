181e6cde4d76ac9fc4d38600836f531311965be0
TurdiyevFirdavsbekIbnQudrat <f1rdavsb3kturdiy3vibnqudrat@gmail.com>
2023-06-15 17:46:56 +0500

    Добавьте файлы проекта.

---
+++ Libraryyyy/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+
+namespace Libraryyyy
+{
+   
+   public class Person
+        {
+
+        public Person? jufti;
+        public bool Oilalimi { get; set; }
+        public string Ism { get; set; }
+
+        public static void OilaMumkinmi(Person p1, Person p2)
+        {   
+            
+                if((p1.Oilalimi==false) && (p2.Oilalimi == false))
+            {
+                p1.jufti = p2;
+                p2.jufti = p1;
+                
+            }
+            else
+            {
+                if (p1.Oilalimi == true)
+                    Console.WriteLine($"{p1.Ism} Oilali");
+                  
+                if (p2.Oilalimi == true )
+                    Console.WriteLine($"{p2.Ism} Oilali");
+            }
+        }
+
+        public void OilasiBor(Person person) 
+        {  
+                Console.WriteLine($"{person.jufti.Ism} va {this.jufti.Ism} turmush qurishdi"); 
+        }
+        }
+}
---
+++ Static/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Libraryyyy;
+using System.Net;
+
+Person Yigit=new Person();
+Person Qiz=new Person();
+Yigit.Ism = "Umid";
+Qiz.Ism = "Gulshanoy";
+
+Yigit.Oilalimi = false;
+Qiz.Oilalimi = false;
+
+Person.OilaMumkinmi(Yigit, Qiz);
+Yigit.OilasiBor(Qiz);
